4584255a652e5b1cc4ad254d8356534ec2a1de3b
ChungYujoyce <joyce.bhps@gmail.com>
2024-04-26 00:43:52 -0700

    add llama3 vllm chorma instructorEmb
    Signed-off-by: ChungYujoyce <joyce.bhps@gmail.com>

--- 2.Pinecone -  HybridRetriever - Adv.Ingestion/ingest.py
+++ 2.Pinecone -  HybridRetriever - Adv.Ingestion/ingest.py
@@ -4,15 +4,30 @@ import asyncio
 import argparse
 
 from dotenv import load_dotenv
-from pinecone import Pinecone, PodSpec
+# from pinecone import Pinecone, PodSpec
 
 from llama_index.core import SimpleDirectoryReader
-from llama_index.llms.openai import OpenAI
 from llama_index.readers.file import UnstructuredReader
 from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core.node_parser import SentenceSplitter
-from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.llms.openai_like import OpenAILike
+# from llama_index.vector_stores.pinecone import PineconeVectorStore
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core import StorageContext
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.instructor import InstructorEmbedding
+
+from chromadb.config import Settings
+import chromadb
+
+CHROMA_SETTINGS = Settings(
+    anonymized_telemetry=False,
+    is_persistent=True,
+)
+
+
 from llama_index.core.extractors import (
     TitleExtractor,
     # QuestionsAnsweredExtractor,
@@ -22,35 +37,24 @@ from llama_index.core.extractors import (
 from llama_parse import LlamaParse
 
 load_dotenv()
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+# openai.api_key = os.environ.get("OPENAI_API_KEY")
+# pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 llama_parse_api_key = os.environ.get("LLAMA_PARSE_API_KEY")
 
-MODEL = os.getenv("MODEL", "gpt-4-0125-preview")
-EMBEDDING = os.getenv("EMBEDDING", "text-embedding-3-large")
+# MODEL = os.getenv("MODEL", "gpt-4-0125-preview")
+EMBEDDING = "hkunlp/instructor-xl"
 
-
-def get_pinecone_index(pc, index_name):
-    pinecone_index = pc.Index(index_name)
-    return pinecone_index
-
-
-def get_pinecone_vector_store(pinecone_index):
-    vector_store = PineconeVectorStore(
-        pinecone_index=pinecone_index,
-        add_sparse_vector=True,
-    )
+def create_chroma_vector_store(path):
+    chroma_client = chromadb.PersistentClient("./chroma_db")
+    chroma_collection = chroma_client.create_collection("quickstart")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
     return vector_store
 
-
-def create_pinecone_pod(pc, index_name):
-    print("Creating pinecone pod")
-    pc.create_index(
-        name=index_name,
-        dimension=3072,
-        metric="dotproduct",
-        spec=PodSpec(environment="gcp-starter"),
-    )
+def load_chroma_vector_store():
+    chroma_client = chromadb.PersistentClient("./chroma_db")
+    chroma_collection = chroma_client.get_collection("quickstart")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    return vector_store
 
 
 def get_documents(input_dir):
@@ -60,8 +64,8 @@ def get_documents(input_dir):
 
     file_extractor = {
         ".pdf": llama_parser,
-        ".html": UnstructuredReader(),
-        ".txt": UnstructuredReader(),
+        # ".html": UnstructuredReader(),
+        # ".txt": UnstructuredReader(),
     }
     print("Reading directory")
     director_reader = SimpleDirectoryReader(
@@ -76,45 +80,45 @@ def run_pipeline(documents, vector_store, llm, num_workers):
     pipeline = IngestionPipeline(
         transformations=[
             SentenceSplitter(chunk_size=512, chunk_overlap=126),
-            TitleExtractor(llm=llm, num_workers=num_workers),
+            # [TODO] TitleExtractor(llm=llm, num_workers=num_workers),
             # QuestionsAnsweredExtractor(questions=3, llm=llm, num_workers=num_workers),
             # SummaryExtractor(
             #    summaries=["prev", "self"], llm=llm, num_workers=num_workers
             # ),
             # KeywordExtractor(keywords=5, llm=llm, num_workers=num_workers),
-            OpenAIEmbedding(model=EMBEDDING),
+            # HuggingFaceEmbedding(model_name=EMBEDDING),
+            InstructorEmbedding(model_name=EMBEDDING),
         ],
         vector_store=vector_store,
     )
     for doc in documents:  # Small patch to remove last_accessed_date from metadata
         k = vars(doc)
-        del k["metadata"]["last_accessed_date"]
+        del k["metadata"]["last_modified_date"]
     pipeline.run(documents=documents, show_progress=True, num_workers=num_workers)
 
 
-async def main():
+def main():
     print("Starting ingestion")
-    input_dir = "./data/source_files/"
+    input_dir = "./data/"
     index_name = "rag-index"
     num_cores = os.cpu_count()
-    num_workers = min(4, num_cores)
-    pc = Pinecone(api_key=pinecone_api_key)
-    parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument(
-        "--gen",
-        action="store_true",
-        help="Generate new pinecone index",
-    )
-    args = parser.parse_args()
-    if args.gen:
-        create_pinecone_pod(pc, index_name)
-    llm = OpenAI(temperature=0.1, model=MODEL, max_tokens=1024)
-    pinecone_index = get_pinecone_index(pc, index_name)
-    vector_store = get_pinecone_vector_store(pinecone_index)
+    # num_workers = min(4, num_cores)
+    num_workers = 1
+
+    # llm = OpenAI(temperature=0.1, model=MODEL, max_tokens=1024)
+    # llm = OpenAILike(
+    #     model="llama3-8b-instruct", 
+    #     api_base="http://vllm:8000/v1", 
+    #     api_key="fake",
+    #     temperature=0.0,
+    # )
+    llm = None
+
+    vector_store = create_chroma_vector_store("./chroma_db")
     documents = get_documents(input_dir)
     print("Starting ingestion pipeline")
     run_pipeline(documents, vector_store, llm, num_workers)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- 2.Pinecone -  HybridRetriever - Adv.Ingestion/main.py
+++ 2.Pinecone -  HybridRetriever - Adv.Ingestion/main.py
@@ -1,31 +1,66 @@
 import os
-import openai
 import chainlit as cl
 
-from pinecone import Pinecone
-from llama_index.core import Settings, VectorStoreIndex
-from llama_index.llms.openai import OpenAI
-from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.vector_stores.pinecone import PineconeVectorStore
+from llama_index.core import Settings, VectorStoreIndex,SimpleDirectoryReader, StorageContext, PromptTemplate
+from llama_index.llms.openai_like import OpenAILike
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.embeddings.instructor import InstructorEmbedding
+from llama_index.vector_stores.chroma import ChromaVectorStore
+import chromadb
 
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+# openai.api_key = os.environ.get("OPENAI_API_KEY")
+# pinecone_api_key = os.environ.get("PINECONE_API_KEY")
 
-MODEL = os.getenv("MODEL", "gpt-4-0125-preview")
-EMBEDDING = os.getenv("EMBEDDING", "text-embedding-3-large")
+# MODEL = os.getenv("MODEL", "gpt-4-0125-preview")
+# EMBEDDING = os.getenv("EMBEDDING", "text-embedding-3-large")
+SYS_PROMPT = PromptTemplate(
+    """\
+<|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
+As a helpful assistant, you will utilize the provided document to answer user questions. 
+Read the given document before providing answers and think step by step. 
+The document has an order of paragraphs with a higher correlation to the questions from the top to the bottom. 
+The answer may be hidden in the tables, so please find it as closely as possible. 
+Do not use any other information to answer the user. Provide a etailed answer to the question.
+Also, please provide the answer in the following order of priorities if applicable:
+Firstly, emphasize GPU characteristics and GPU products.
+Secondly, Give prominence to power-related specifications such as fan cooling or liquid cooling, power consumption, and so on.
+Thirdly, If applicable, mention green computing.
+Remember, please don't provide any fabricated information, ensuring that everything stated is accurate and true.
+
+Context information is below.
+---------------------
+{context_str}
+---------------------
+Given the context information and not prior knowledge, answer the query.<|eot_id|>
+{query_str}\
+"""
+)
+CONVERSATION_PROMPT = "<|start_header_id|>{role}<|end_header_id|>\n\n{content}<|eot_id|>"
+RESPONSE_PROMPT = "<|start_header_id|>{role}<|end_header_id|>\n\n"
+EMBEDDING = os.getenv("EMBEDDING", "hkunlp/instructor-xl")
+
+def load_chroma_vector_store():
+    chroma_client = chromadb.PersistentClient("./chroma_db")
+    chroma_collection = chroma_client.get_collection("quickstart")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+
+    return vector_store
 
 @cl.cache
 def load_context():
-    Settings.llm = OpenAI(temperature=0.1, model=MODEL, streaming=True)
-    Settings.embed_model = OpenAIEmbedding(model=EMBEDDING, embed_batch_size=1)
-    Settings.num_output = 1024
-    Settings.context_window = 128000
-    pc = Pinecone(api_key=pinecone_api_key)
-    pinecone_index = pc.Index("rag-index")
-    vector_store = PineconeVectorStore(
-        pinecone_index=pinecone_index,
+    Settings.llm = OpenAILike(
+        model="llama3-8b-instruct", 
+        api_base="http://vllm:8000/v1", 
+        api_key="fake",
+        temperature=0.0,
+        max_tokens=256,
     )
+    Settings.embed_model = InstructorEmbedding(model_name="hkunlp/instructor-xl")
+    Settings.num_output = 1024
+    Settings.context_window = 8192
+    
+    vector_store = load_chroma_vector_store()
 
     index = VectorStoreIndex.from_vector_store(
         vector_store=vector_store,
@@ -40,6 +75,7 @@ async def start():
     query_engine = index.as_query_engine(
         streaming=True,
         similarity_top_k=4,
+        text_qa_template=SYS_PROMPT,
         vector_store_query_mode="hybrid",
     )
     cl.user_session.set("query_engine", query_engine)
@@ -48,7 +84,7 @@ async def start():
     cl.user_session.set("message_history", message_history)
 
     await cl.Message(
-        author="Assistant", content="Hello! Im an AI assistant. How may I help you?"
+        author="assistant", content="Hello! Im an AI assistant. How may I help you?"
     ).send()
 
 
@@ -73,31 +109,40 @@ async def set_sources(response, response_message):
 
 
 @cl.on_message
-async def main(message: cl.Message):
+async def main(user_message: cl.Message):
+    n_history_messages = 4
     query_engine = cl.user_session.get("query_engine")
     message_history = cl.user_session.get("message_history")
-    prompt_template = "Previous messages:\n"
+    prompt_template = ""
+    print(message_history)
+    for past_message in message_history:
+        prompt_template += CONVERSATION_PROMPT.format(
+            role=past_message['author'],
+            content=past_message['content'],
+        )
 
-    response_message = cl.Message(content="", author="Assistant")
-
-    user_message = message.content
-
-    for message in message_history:
-        prompt_template += f"{message['author']}: {message['content']}\n"
-    prompt_template += f"Human: {user_message}"
+    prompt_template += CONVERSATION_PROMPT.format(
+        role="user",
+        content=user_message.content,
+    ) 
+    
+    prompt_template += RESPONSE_PROMPT.format(
+        role='assistant',
+    ) 
 
     response = await cl.make_async(query_engine.query)(prompt_template)
-
+    
+    assistant_message = cl.Message(content="", author="assistant")
     for token in response.response_gen:
-        await response_message.stream_token(token)
+        await assistant_message.stream_token(token)
     if response.response_txt:
-        response_message.content = response.response_txt
-    await response_message.send()
+        assistant_message.content = response.response_txt
+    await assistant_message.send()
 
-    message_history.append({"author": "Human", "content": user_message})
-    message_history.append({"author": "AI", "content": response_message.content})
-    message_history = message_history[-4:]
+    message_history.append({"author": "user", "content": user_message.content})
+    message_history.append({"author": "assistant", "content": assistant_message.content})
+    message_history = message_history[-n_history_messages:]
     cl.user_session.set("message_history", message_history)
 
     if response.source_nodes:
-        await set_sources(response, response_message)
+        await set_sources(response, assistant_message)
